368fd1cf6ff1869ee728ac032b827c34d0f89b26
xxassafxx <57146087+xxassafxx@users.noreply.github.com>
2019-12-08 23:54:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,7 +46,7 @@ client.on('message', message => {
 if (message.content === prefix+'spam') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
+      for(let x = 0; x < 9000000000000000000000; x++) {
         message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
           .then(m => {
             count++;
