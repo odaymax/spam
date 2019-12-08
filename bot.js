4e0f4b9d492e48f8d7306c2852b747c7a944a88e
xxassafxx <57146087+xxassafxx@users.noreply.github.com>
2019-12-08 22:14:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,19 +86,12 @@ client2.on('message', message => {
     }
 });
 
-client2.on('message', message => {
-if (message.content === prefix+'spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 9000000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
+client2.on("ready", () => {
+let channel =     client.channels.get("652758616444502037")
+setInterval(function() {
+channel.send(`طااااااااااااط سباااااااااااااااااااممممممممممممم `);
+}, 30)
+})
 
 client2.on('message', message => {
   if (message.author.bot) return;
@@ -121,3 +114,4 @@ if (!rank) return message.reply(' ')
 
 
 client.login(process.env.BOT_TOKEN);
+client2.login(process.env.BOT_TOKEN);
